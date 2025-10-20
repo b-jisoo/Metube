06ecd2766b3b38165446dfcd3079d8d8c1d7d900
b-jisoo <mrk334kr@naver.com>
2025-10-21 07:42:56 +0900

    feat(trpc): set up tRPC configuration and apply rate limiting middleware

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { ClientGreeting } from "./client-greeting";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+
+export default async function Home() {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery(trpc.hello.queryOptions({ text: "world" }));
+
   return (
     <>
-      <div>I will load videos in the future!</div>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <ErrorBoundary fallback={<div>Something went wrong</div>}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <ClientGreeting />
+          </Suspense>
+        </ErrorBoundary>
+      </HydrationBoundary>
     </>
   );
 }
